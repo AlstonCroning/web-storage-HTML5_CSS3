1e4cc41e7d24c23c0725914f0a67cf15547f39bb
Alston <and_croning@hotmail.com>
2015-07-10 16:00:34 -0400

    Using the 'Query Selector All' method with Javascript

--- Query_Selector/javascript.js
+++ Query_Selector/javascript.js
@@ -1,6 +1,13 @@
 
 function bringit(){
-	document.querySelector('#first').onclick=say;
+	var list = document.querySelectorAll('#first');
+	list[1].onclick=say;//single query selects here
+/*	
+	//all of them selected of the same id name "first"
+	for(var i=0; i < list.length; i++){
+		list[i].onclick=say;
+	}
+*/
 }
 
 function say(){
