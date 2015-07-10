ddf03c796e210ece06992ce08c939ec99c670957
Alston <and_croning@hotmail.com>
2015-07-10 02:37:00 -0400

    commented a code block in js file

--- Query_Selector/javascript.js
+++ Query_Selector/javascript.js
@@ -8,3 +8,16 @@ function say(){
 }
 
 window.onload=bringit;//when the page loads
+
+/*
+//using query selector
+	function bringit(){
+		document.querySelector('#first').onclick=say;
+	}
+
+	function say(){
+		alert('you clicked something!');
+	}
+
+	window.onload=bringit;//when the page loads
+*/
