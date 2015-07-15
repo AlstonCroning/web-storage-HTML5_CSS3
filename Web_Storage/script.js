d8dd2140b2b7e805d2b38ca94ac6baa5d0d4feb3
Alston <and_croning@hotmail.com>
2015-07-14 23:16:07 -0400

    Web Storage API final built

--- Web_Storage/script.js
+++ Web_Storage/script.js
@@ -1,24 +1,33 @@
-function doit(){
+//when the page loads run doit function
+window.addEventListener("load", doit, false);
 
+//when the 'save' button clicks run the saveit function
+function doit(){
 	var button = document.getElementById("button");
 	button.addEventListener("click", saveit, false);
-
+	display();
 }
 
+//after the 'save' button clicked store field information
 function saveit(){
 	var one = document.getElementById("one").value;
 	var two = document.getElementById("two").value;
-	
 	sessionStorage.setItem(one, two);
-	
-	display(one);
+	display();
+	document.getElementById("one").value = "";
+	document.getElementById("two").value = "";
 }
 
-function display(one){
-	
+//display the results in the rightbox div
+function display(){	
 	var rightbox = document.getElementById("rightbox");
-	var two = sessionStorage.getItem(one);
-	rightbox.innerHTML = "The variable name is: "+one+"<br />and the value is: "+two;
+	rightbox.innerHTML = "";
+	
+	for (var x = 0; x < sessionStorage.length; x++ ) {
+		var a = sessionStorage.key(x);
+		var b = sessionStorage.getItem(a);
+		rightbox.innerHTML += a+" / "+b+"<br />";
+	}
+	//	var two = sessionStorage.getItem(one);
+	//	rightbox.innerHTML = "The variable name is: "+one+"<br />and the value is: "+two;
 }
-
-window.addEventListener("load", doit, false);
